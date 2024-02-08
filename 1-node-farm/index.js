95fd8ced400ae945c916b26633045b2417ff74f6
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-02-08 22:11:52 +0200

    Lesson 13

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -2,54 +2,28 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-//////////////////////////////////////////////////////////////////////
-//? FILES
-// const hello = 'Hello World';
-// console.log(hello);
-
-//! Blocking, synchronous way
-// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
-// console.log(textIn);
-// const textOut = `This is what we know about avocado: ${textIn}. \nCreated on ${Date.now()}`
-// fs.writeFileSync('./txt/output.txt', textOut)
-// console.log('File written!');
-
-//! Non-blocking, asynchronous way
-// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-//     if (err) {
-//         return console.log('Error!!!');
-//     }
-//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-//         console.log(data2);
-//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-//             console.log(data3);
-//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-//                 console.log('file wrtitten!');
-//             })
-//         })
-//     });
-// });
-// console.log('Will read file');
-
-
 //? SERVER
 //! Web Server
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
-    // console.log(req);
-    console.log(req.url);
     const pathName = req.url;
+
     if (pathName === '/' || pathName === '/overview') {
-        res.end('This is the OVERVIEW')
+        res.end('This is the OVERVIEW');
     } else if (pathName === '/product') {
-        res.end('This is the PRODUCT')
+        res.end('This is the PRODUCT');
+    } else if (pathName === '/api') {
+        res.writeHead(200, { 'Content-type': 'application/json' });
+        res.end(data);
     } else {
         res.writeHead(404, {
-            'Contetn-type': 'text/html',
+            'Content-Type': 'text/html',
             'my-own-header': 'hello-world'
         });
-        res.end('<h1>Page not found!</h1>')
+        res.end('<h1>Page not found!</h1>');
     }
-    // res.end('Hello from the server')
 });
 
 server.listen(8000, '127.0.0.1', () => {
