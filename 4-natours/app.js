cdadaf565be885a7647e3b91912388016c3b7f77
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-02-12 13:58:56 +0200

    52 Done

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,17 +1,33 @@
+const fs = require('fs');
 const express = require('express');
+const { start } = require('repl');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf8')
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
-});
 app.listen(3000, () => {
   console.log(`App running on port ${port}`);
 });
