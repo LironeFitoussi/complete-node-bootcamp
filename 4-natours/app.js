3a670d2c1c385c803bfd2cbd97c2233db524a855
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-02-12 16:20:39 +0200

    ES6 module

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,21 +1,32 @@
-const fs = require('fs');
-const express = require('express');
-const { start } = require('repl');
+import fs from 'fs';
+import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log('Hello from the middleware!');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(
-    `${__dirname}/dev-data/data/tours-simple.json`
+    `./dev-data/data/tours-simple.json`
   )
 );
 
 const getAllTours = (req, res) => {
+  //   console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
