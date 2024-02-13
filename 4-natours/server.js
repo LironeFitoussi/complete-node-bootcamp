e45b19e1330f5672a3da62a92f4d8219a24de56c
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-02-13 13:05:21 +0200

    progress

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,8 +1,14 @@
+const mongoose = require('mongoose')
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
-
-const app = require('./app');
+const db = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(con => console.log('db connection successful'))
 
 const port = process.env.PORT || 3000;
 app.listen(3000, () => {
