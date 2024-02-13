494ca40bdb6971cfb34912a650d9364eeddcf14d
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-02-13 10:19:27 +0200

    Section 6 Done

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -6,7 +6,7 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-console.log(process.env.NODE_ENV);
+// console.log(process.env.NODE_ENV);
 
 // 1) Middlewares
 if (process.env.NODE_ENV === 'development') {
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
-  console.log('Hello from the middleware!');
+  // console.log('Hello from the middleware!');
   next();
 });
 
