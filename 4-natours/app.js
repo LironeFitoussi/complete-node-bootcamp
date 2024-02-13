d21167a822cc7d3baa8814a62f7ad7a8d86c9710
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-02-13 10:01:39 +0200

    Progress

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) Middleware Handeling
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+
+// 1) Middlewares
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   console.log('Hello from the middleware!');
   next();
