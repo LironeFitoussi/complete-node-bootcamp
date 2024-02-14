4209b7153618366557fb0dd3dec20ab10c272732
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-02-14 10:56:27 +0200

    Deleting Tour Done

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -80,10 +80,18 @@ exports.updtaeTour = async (req, res) => {
   
 };
 
-// TODO
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+//! Pending
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id)
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message,
+    })
+  }
 };
