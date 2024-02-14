e7060a746f1908d5659efa470c7a55ca62400386
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-02-14 08:00:42 +0200

    node progress on app ans server js

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -10,6 +10,36 @@ mongoose.connect(db, {
   useFindAndModify: false
 }).then(con => console.log('db connection successful'))
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a tour name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'Please add a tour price']
+  }
+})
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  // rating: 4.7,
+  price: 997
+})
+
+testTour.save().then((doc) => {
+  console.log(doc);
+}).catch((err) => {
+  console.log("Error! : " + err);
+})
+
 const port = process.env.PORT || 3000;
 app.listen(3000, () => {
   console.log(`App running on port ${port}`);
