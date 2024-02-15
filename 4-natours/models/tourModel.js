d6481835a37136f2f2900fa1c558268504c5f298
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-02-15 11:05:24 +0200

    Agreegation middleware

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -97,8 +97,15 @@ tourSchema.pre(/^find/, function (next) {
 });
 
 tourSchema.post(/^find/, function (docs, next) {
-  console.log(docs);
+  // console.log(docs);
   console.log(`Query took ${Date.now() - this.start} ms`);
+  next();
+});
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
   next();
 });
 
