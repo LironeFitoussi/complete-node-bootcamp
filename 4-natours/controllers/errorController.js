d4e87bc3d42891adbcbba183053b4f81dd204bad
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-02-21 21:24:53 +0200

    handle prod error

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -1,3 +1,9 @@
+const AppError = require('../utils/appError');
+const handlecastErrorDB = (err) => {
+  const message = `Invalid ${err.path} : ${err.value}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -34,6 +40,8 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    if (err.name === 'CastError') error = handlecastErrorDB(error);
+    sendErrorProd(error, res);
   }
 };
