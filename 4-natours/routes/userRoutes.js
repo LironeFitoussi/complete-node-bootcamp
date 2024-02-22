d3e154602dffcde332a37d3073518047dccec8d1
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-02-22 18:58:27 +0200

    user deleting and showing only active user

--- 4-natours/routes/userRoutes.js
+++ 4-natours/routes/userRoutes.js
@@ -8,13 +8,13 @@ router.route('/login').post(authController.login);
 router.route('/forgotPassword').post(authController.forgotPassword);
 router.route('/resetPassword/:token').patch(authController.resetPassword);
 
-router
-  .route('/updateMyPassword')
-  .patch(authController.protect, authController.updatePassword);
-
-router
-  .route('/updateMe')
-  .patch(authController.protect, userController.updateMe);
+router.patch(
+  '/updateMyPassword',
+  authController.protect,
+  authController.updatePassword,
+);
+router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
 router
   .route('/')
