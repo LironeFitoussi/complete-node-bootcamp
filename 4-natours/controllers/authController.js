159a0f9d61a4b3ff8789125df1ee72ad20bfada6
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-02-22 13:38:26 +0200

    mail reset updated

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const sendEmail = require('../utils/email');
 // const express = require('express');
 
 const signToken = (id) => {
@@ -133,6 +134,31 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   // 2) Generate the random token
   const resetToken = await user.createPasswordResetToken();
   await user.save({ validateBeforeSave: false });
+
   // 3) Send it to user's email
+  const resetURL = `${req.protocol}//${req.get('host')}/api/v1//users/resetPassword/${resetToken}`;
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password Reset',
+      text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n
+      Please click on the following link, or paste this into your browser to complete the process:\n\n
+      ${resetURL}\n\n
+      If you did not request this, please ignore this email and your password will remain unchanged.\n`,
+    });
+    res.status(200).json({
+      status: 'success',
+      message: 'An e-mail has been sent to you with further instructions.',
+    });
+  } catch (error) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError('There was an error sending the email, Try again later'),
+      500,
+    );
+  }
 });
 exports.resetPassword = catchAsync(async (req, res, next) => {});
