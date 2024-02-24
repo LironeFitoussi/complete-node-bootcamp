a069d252c743bafa4d3ce47cc7773e52bd81d495
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-02-24 22:28:48 +0200

    sdfgdsfg

--- 4-natours/routes/reviewRoutes.js
+++ 4-natours/routes/reviewRoutes.js
@@ -3,9 +3,6 @@ const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
-
-router.use(authController.protect);
-
 router
   .route('/')
   .get(reviewController.getAllReviews)
