248f0e5d5808df79a1453074a150935e83b4ebab
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-02-24 21:16:51 +0200

    virtual populating

--- 4-natours/models/reviewModel.js
+++ 4-natours/models/reviewModel.js
@@ -34,10 +34,14 @@ const reviewSchema = new mongoose.Schema(
 );
 
 reviewSchema.pre(/^find/, function (next) {
+  // this.populate({
+  //   path: 'user',
+  //   select: 'name photos',
+  // }).populate({
+  //   path: 'tour',
+  //   select: 'name',
+  // });
   this.populate({
-    path: 'user',
-    select: 'name photos',
-  }).populate({
     path: 'tour',
     select: 'name',
   });
