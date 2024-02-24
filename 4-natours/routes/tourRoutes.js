30893a4ad42b77e9d0e2a981b62a7a8f554edaf4
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-02-24 21:52:33 +0200

    nested points

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
-const reviewController = require('../controllers/reviewController');
-
+const reviewRouter = require('../routes/reviewRoutes');
 const router = express.Router();
+
+router.use('/:tourId/reviews', reviewRouter);
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
@@ -24,19 +25,5 @@ router
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour,
   );
-
-// POST /tour/1254fdv/reviews/fdsgd5gfn
-// GET /tour/1254fdv/reviews/
-// GET /tour/1254fdv/reviews/fdsgd5gfn
-
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview,
-  );
-
-router.route('/:tourId/reviews/');
 
 module.exports = router;
