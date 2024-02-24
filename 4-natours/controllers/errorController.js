52c7989418cb587cd1e9d6f996a0206ca26401e3
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-02-24 22:38:54 +0200

    sadfasdfsdf
    asdfaeawefawe

---
+++ 4-natours/controllers/errorController.js
@@ -0,0 +1,76 @@
+const { log } = require('util');
+const AppError = require('../utils/appError');
+const handlecastErrorDB = (err) => {
+  const message = `Invalid ${err.path} : ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldDB = (err) => {
+  const value = err.keyValue['name'];
+  const message = `Duplicated field value :"${value}" Please use another value`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+
+  return new AppError(message, 400);
+};
+
+const handleJWTError = () => {
+  const message = 'Invalid token. Please log in again!';
+  return new AppError(message, 401);
+};
+
+const handleTokenExpiredError = () => {
+  const message = 'Token has expired. Please log in again!';
+  return new AppError(message, 401);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // Programing or other unknowm error: don't leak error details
+  } else {
+    // 1) Log error
+    console.error('ERROR 💥', err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: err.status,
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    console.log(error);
+    if (error.name === 'CastError') error = handlecastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldDB(error);
+    if (error._message === 'Validation failed') error = handleValidationErrorDB(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    if (error.name === 'TokenExpiredError') error = handleTokenExpiredError();
+    sendErrorProd(error, res);
+  }
+};
---
+++ 4-natours/controllers/userController.js
@@ -0,0 +1,88 @@
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const filteredObj = {};
+  Object.keys(obj).forEach((key) => {
+    if (allowedFields.includes(key)) {
+      filteredObj[key] = obj[key];
+    }
+  });
+  return filteredObj;
+};
+exports.getAllUsers = async (req, res, next) => {
+  const users = await User.find();
+
+  // SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'fail',
+    message: 'Not Implemented',
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'fail',
+    message: 'Not Implemented',
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'fail',
+    message: 'Not Implemented',
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'fail',
+    message: 'Not Implemented',
+  });
+};
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if userPOSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    console.log('error');
+    return next(
+      new AppError(
+        'This route is not for password updates. Please us /updateMyPassword ',
+        400,
+      ),
+    );
+  }
+  // 2) Update the user document
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  //
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
