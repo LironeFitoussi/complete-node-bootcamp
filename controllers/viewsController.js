ec921da4c074c377104db3d2566f0ede39fb4d04
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-02-28 11:17:53 +0200

    Lesson 183 Done

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,17 @@
-exports.getOverview = (req, res) => {
+const Tour = require("./../models/tourModel");
+const catchAsync = require("./../utils/catchAsync");
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour Data from Collection
+  const tours = await Tour.find({});
+
+  // 2) Build tour overview template
+  // 3) Render Template using tour data from step 1
   res.status(200).render("overview", {
     title: "All Tours",
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render("tour", {
