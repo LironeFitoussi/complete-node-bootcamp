2432a56e356e567e4990bb83972fa278326d582c
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-02-28 13:23:13 +0200

    185 done

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -2,19 +2,22 @@ const Tour = require("./../models/tourModel");
 const catchAsync = require("./../utils/catchAsync");
 
 exports.getOverview = catchAsync(async (req, res, next) => {
-  // 1) Get tour Data from Collection
   const tours = await Tour.find({});
 
-  // 2) Build tour overview template
-  // 3) Render Template using tour data from step 1
   res.status(200).render("overview", {
     title: "All Tours",
     tours,
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: "reviews",
+    fields: "reaview rating user",
+  });
+
   res.status(200).render("tour", {
     title: "The Forest Hiker",
+    tour,
   });
-};
+});
