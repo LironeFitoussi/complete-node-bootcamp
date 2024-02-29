a5e733522b1ebbba96611315bf74e2900f95d678
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-02-29 14:18:12 +0200

    html post update version

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,6 +1,8 @@
 const Tour = require("./../models/tourModel");
+const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
+
 exports.getOverview = catchAsync(async (req, res, next) => {
   const tours = await Tour.find({});
 
@@ -34,5 +36,25 @@ exports.getLoginForm = catchAsync(async (req, res) => {
 exports.getAccount = catchAsync(async (req, res) => {
   res.status(200).render("account", {
     title: "Your Account",
+  });
+});
+
+exports.updateUserData = catchAsync(async (req, res, next) => {
+  // console.log("UPDATING USER ", req.body);
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  res.status(200).render("account", {
+    title: "Your Account",
+    user: updatedUser,
   });
 });
