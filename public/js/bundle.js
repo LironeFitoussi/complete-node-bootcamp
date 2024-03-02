b4d874b25dd4acf770d7cff48271f0fd580e57c2
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-03-02 23:43:32 +0200

    sdfsdf

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12484,7 +12484,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55826" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49973" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
