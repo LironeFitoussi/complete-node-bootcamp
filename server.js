a53a9be95e162b969154f5e3137fde4392201179
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-03-13 07:40:11 +0200

    Revert "Implemented cors"
    This reverts commit d7a7ee0b9a9f23ecb7035ee5d0b3eacda1243fc4.

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ process.on("unhandledRejection", (err) => {
 });
 
 process.on("SIGTERM", () => {
-  console.log("👋 SIGTERM RECEIVED. Shutting down gracefully");
+  console.log("👋SIGTERM REQCIVED. Shutting down gracefully");
   server.close(() => {
     console.log("💥 Process terminated!");
   });
